ce3698d2a6000bed86cff6f6a990c462c2a7ce89
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-26 13:09:27 +0200

    ✨ Adding AuthorizedServerCredential setting up automatically given request.

---
+++ src/Credential/AuthorizedServerCredential.php
@@ -0,0 +1,20 @@
+<?php
+namespace Deegitalbe\ServerAuthorization\Credential;
+
+use Deegitalbe\ServerAuthorization\Facades\Package;
+use Henrotaym\LaravelApiClient\Contracts\RequestContract;
+use Henrotaym\LaravelApiClient\Contracts\CredentialContract;
+
+class AuthorizedServerCredential implements CredentialContract {
+    /**
+     * Preparing request.
+     */
+    public function prepare(RequestContract &$request)
+    {
+        $request->addHeaders([
+            Package::header() => Package::authorization(),
+            'X-Requested-With' => "XMLHttpRequest",
+            'Content-Type' => "application/json"
+        ]);
+    }
+}
